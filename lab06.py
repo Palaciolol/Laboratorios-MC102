41e5bcd939c20cda02376bc25f54672eaafd2d30
Lucas_Palacio <132016588+Palaciolol@users.noreply.github.com>
2023-05-05 00:28:01 -0300

    lab6 versao final

--- lab06.py
+++ lab06.py
@@ -1,4 +1,4 @@
-def soma_vetores(vetor1: list[int], vetor2: list[int]):
+def soma_vetores(vetor1: list[int], vetor2: list[int]) -> list[int]:
     '''Dadas duas listas de inteiros, soma elemento a elemento das listas
 
     Parametros:
@@ -12,21 +12,23 @@ def soma_vetores(vetor1: list[int], vetor2: list[int]):
             soma.append(x+y)
 
     elif len(vetor1) > len(vetor2):
+        for i in range(0, len(vetor1) - len(vetor2)):
+            vetor2.append(0)
+
         for x, y in soma_zip:
             soma.append(x+y)
-
-        soma.append(vetor1(-1))
 
     elif len(vetor2) > len(vetor1):
+        for i in range(0, len(vetor2) - len(vetor1)):
+            vetor1.append(0)
+
         for x, y in soma_zip:
             soma.append(x+y)
 
-        soma.append(vetor2(-1))
-
-    print(soma)
+    return soma
 
 
-def subtrai_vetores(vetor1: list[int], vetor2: list[int]):
+def subtrai_vetores(vetor1: list[int], vetor2: list[int]) -> list[int]:
     '''Dadas duas listas de inteiros,subtrai elemento a elemento das listas
 
     Parametros:
@@ -41,21 +43,23 @@ def subtrai_vetores(vetor1: list[int], vetor2: list[int]):
             subtracao.append(x-y)
 
     elif len(vetor1) > len(vetor2):
+        for i in range(0, len(vetor1) - len(vetor2)):
+            vetor2.append(0)
+
         for x, y in subtrai_zip:
             subtracao.append(x-y)
-
-        subtracao.append(vetor1(-1))
 
     elif len(vetor2) > len(vetor1):
+        for i in range(0, len(vetor2) - len(vetor1)):
+            vetor1.append(0)
+
         for x, y in subtrai_zip:
             subtracao.append(x-y)
 
-        subtracao.append(vetor2(-1))
-
-    print(subtracao)
+    return subtracao
 
 
-def multiplica_vetores(vetor1: list[int], vetor2: list[int]):
+def multiplica_vetores(vetor1: list[int], vetor2: list[int]) -> list[int]:
     '''Dadas duas listas de inteiros, multiplica elemento a elemento das listas
 
     Parâmetros:
@@ -71,23 +75,25 @@ def multiplica_vetores(vetor1: list[int], vetor2: list[int]):
             multiplicacao.append(x*y)
 
     elif len(vetor1) > len(vetor2):
+        for i in range(0, len(vetor1) - len(vetor2)):
+            vetor2.append(1)
+
         for x, y in multiplica_zip:
             multiplicacao.append(x*y)
-
-        multiplicacao.append(vetor1(-1))
 
     elif len(vetor2) > len(vetor1):
+        for i in range(0, len(vetor2) - len(vetor1)):
+            vetor1.append(1)
+
         for x, y in multiplica_zip:
             multiplicacao.append(x*y)
 
-        multiplicacao.append(vetor2(-1))
-
-    print(multiplicacao)
+    return multiplicacao
 
 
-def divide_vetores(vetor1: list[int], vetor2: list[int]):
-    ''''Dadas duas listas de inteiros, faz a divisão inteira elemento a elemento das listas
-
+def divide_vetores(vetor1: list[int], vetor2: list[int]) -> list[int]:
+    '''Dadas duas listas de inteiros faz a divisão inteira elemento a elemen-
+    to das listas
     Parâmetros:
     vetor1--lista de inteiros
     vetor2--lista de inteiros
@@ -101,22 +107,26 @@ def divide_vetores(vetor1: list[int], vetor2: list[int]):
             divisao.append(x//y)
 
     elif len(vetor1) > len(vetor2):
+        for i in range(0, len(vetor1) - len(vetor2)):
+            vetor2.append(1)
+
         for x, y in divide_zip:
             divisao.append(x//y)
-
-        divisao.append(1)
 
     elif len(vetor2) > len(vetor1):
+        for i in range(0, len(vetor2) - len(vetor1)):
+            vetor1.append(0)
+
         for x, y in divide_zip:
             divisao.append(x//y)
 
-        divisao.append(0)
-
-    print(divisao)
+    return divisao
 
 
-def multiplicacao_escalar(vetor: list[int], escalar: int):
-    '''Dada uma lista e um número inteiro, multiplica todos os elementos da lista pelo números inteiro
+def multiplicacao_escalar(vetor: list[int],
+                          escalar: int) -> list[int]:
+    '''Dada uma lista e um número inteiro, multiplica todos os elementos da lis
+    ta pelo números inteiro
 
     Parâmetros:
     vetor--lista de inteiros
@@ -124,91 +134,114 @@ def multiplicacao_escalar(vetor: list[int], escalar: int):
     '''
     multiplica_escalar = []
 
-    for x in range(0,len(vetor)):
+    for x in range(0, len(vetor)):
         multiplica_escalar.append(escalar*vetor[x])
 
     return multiplica_escalar
 
 
-def n_duplicacao(vetor: list[int], n: int):
+def n_duplicacao(vetor: list[int], n: int) -> list[int]:
     '''Dada uma lista e um número inteiro n, replica a lista n vezes
 
     Parâtros:
     vetor--lista de inteiros
     n--número inteiro maior ou igual a zero
     '''
+    if n > 0:
+        return (vetor*n)
 
-    print(vetor*n)
+    else:
+        return []
 
 
-def soma_elementos(vetor: list[int]):
+def soma_elementos(vetor: list[int]) -> int:
     '''Dada uma lista de inteiros, soma todos os elementos da lista
 
-    Parâmetros:
+    Parâmetros :
     vetor--lista de inteiros
     '''
 
     soma_tudo = 0
-
-    for x in range(0, vetor):
+    for x in range(0, len(vetor)):
         soma_tudo += vetor[x]
 
     return soma_tudo
 
 
-def produto_interno(vetor1: list[int], vetor2: list[int]):
-    '''Dadas duas listas de inteiros, soma a multiplicacão elemento a elemento das listas
-
-    Parâmetros:
+def produto_interno(vetor1: list[int], vetor2: list[int]) -> int:
+    '''Dadas duas listas de inteiros, soma a multiplicacão elemento a elemento
+    das listas
+    Parâmetros :
     vetor1--listta de inteiros
     vetor2--lista de inteiros
     '''
 
-    r1 = multiplica_vetores(vetor1, vetor2)
-    soma_elementos(r1)
+    if len(vetor1) > len(vetor2):
+        for i in range(0, len(vetor1) - len(vetor2)):
+            vetor2.append(1)
+
+        r1 = soma_elementos(multiplica_vetores(vetor1, vetor2))
+
+    elif len(vetor2) > len(vetor1):
+        for i in range(0, len(vetor2) - len(vetor1)):
+            vetor1.append(1)
+
+        r1 = soma_elementos(multiplica_vetores(vetor1, vetor2))
+
+    elif len(vetor1) == len(vetor2):
+        r1 = soma_elementos(multiplica_vetores(vetor1, vetor2))
+
+    return r1
 
 
-def multiplica_todos(vetor1: list[int], vetor2: list[int]):
-    '''Dadas duas listas de inteiros, devolve uma lista na qual os elementos são a soma da multiplicação do primeiro elemento da primeira lista por todos os elementos da segunda lista
-
-    Parâmetros:
+def multiplica_todos(vetor1: list[int], vetor2: list[int]) -> list[int]:
+    '''Dadas duas listas de inteiros, devolve uma lista na qual os elementos
+    são a soma da multiplicação do primeiro elemento da primeira lista por
+    todos os elementos da segunda lista
+    Parâmetros :
     vetor1--lista de inteiros
     vetor2--lista de inteiros
     '''
 
     multi_todos = []
 
-    for x in range(0,len(vetor1)):
-        multiplicacao_escalar(vetor2,vetor1[x])
-        multi_todos.append(soma_elementos(multiplicacao_escalar(vetor2,vetor1[x])))
+    for x in range(0, len(vetor1)):
+        multiplicacao_escalar(vetor2, vetor1[x])
+        multi_todos.append(soma_elementos(
+            multiplicacao_escalar(vetor2, vetor1[x])))
 
-    print(multi_todos)
+    return multi_todos
 
 
-def correlacao_cruzada(vetor: list[int], mascara: list[int]):
-    '''Dadas duas listas, uma maior e outra menor, devolve uma lista na qual os elementos são a soma,até o último elemento da lista menor, da multiplicação elemento a elemento das listas
-
+def correlacao_cruzada(vetor: list[int], mascara: list[int]) -> list[int]:
+    '''Dadas duas listas, uma maior e outra menor, devolve uma
+    lista na qual os elementos são a soma,até o
+    último elemento da lista menor, da
+    multiplicação elemento a elemento das listas
     Parâmetros:
     vetor--lista de inteiros
     mascara--lista de inteiros menor
     '''
-    correlacao= []
-    for x in range(0,len(mascara)):
-        correlacao.append(produto_interno(vetor,mascara))
+
+    correlacao = []
+
+    for x in range(0, len(vetor) - len(mascara) + 1):
+        correlacao.append(produto_interno(vetor[x: len(mascara)+x], mascara))
+
+    return correlacao
 
 
+def transforma_int(vetor_t: list[str]) -> list[int]:
+    vetor_aleatorio = []
+    for i in range(0, len(vetor_t)):
+        vetor_aleatorio.append(int(vetor_t[i]))
+
+    return vetor_aleatorio
 
 
-def transforma_int(list):
-    for i in list:
-        list[i] = int(list[i])
-
-    return list
-
-
-def main():
+def main() -> None:
     vetores = input().split(',')
-    transforma_int(vetores)
+    vetores = transforma_int(vetores)
 
     while True:
         procedimento = input()
@@ -218,49 +251,60 @@ def main():
 
         elif procedimento == 'soma_vetores':
             vetores_soma = input().split(',')
-            transforma_int(vetores_soma)
-            soma_vetores(list[int], vetores_soma)
+            vetores_soma = transforma_int(vetores_soma)
+            vetores = soma_vetores(vetores, vetores_soma)
+            print(vetores)
 
         elif procedimento == 'subtrai_vetores':
             vetores_subtracao = input().split(',')
-            transforma_int(vetores_subtracao)
-            subtrai_vetores(list[int], vetores_subtracao)
+            vetores_subtracao = transforma_int(vetores_subtracao)
+            vetores = subtrai_vetores(vetores, vetores_subtracao)
+            print(vetores)
 
         elif procedimento == 'multiplica_vetores':
             vetores_multiplica = input().split(',')
-            transforma_int(multiplica_vetores)
-            multiplica_vetores(list[int], vetores_multiplica)
+            vetores_multiplica = transforma_int(vetores_multiplica)
+            vetores = multiplica_vetores(vetores, vetores_multiplica)
+            print(vetores)
 
         elif procedimento == 'divide_vetores':
             vetores_divide = input().split(',')
-            transforma_int(divide_vetores)
-            divide_vetores(list[int], vetores_divide)
+            vetores_divide = transforma_int(vetores_divide)
+            vetores = divide_vetores(vetores, vetores_divide)
+            print(vetores)
 
         elif procedimento == 'multiplicacao_escalar':
             multiplica_escalar = int(input())
-            multiplicacao_escalar(list[int], multiplica_escalar)
+            vetores = multiplicacao_escalar(vetores, multiplica_escalar)
+            print(vetores)
 
         elif procedimento == 'n_duplicacao':
             duplicacao = int(input())
-            n_duplicacao(list[int], duplicacao)
+            vetores = n_duplicacao(vetores, duplicacao)
+            print(vetores)
 
         elif procedimento == 'soma_elementos':
-            soma_elementos(list[int])
+            vetores = [soma_elementos(vetores)]
+            print(vetores)
 
         elif procedimento == 'produto_interno':
             vetores_produto_interno = input().split(',')
-            transforma_int(vetores_produto_interno)
-            produto_interno(list[int], vetores_produto_interno)
+            vetores_produto_interno = transforma_int(vetores_produto_interno)
+            vetores = [produto_interno(vetores, vetores_produto_interno)]
+            print(vetores)
 
         elif procedimento == 'multiplica_todos':
             vetores_mtodos = input().split(',')
-            transforma_int(vetores_mtodos)
-            multiplica_todos(list[int], vetores_mtodos)
+            vetores_mtodos = transforma_int(vetores_mtodos)
+            vetores = multiplica_todos(vetores, vetores_mtodos)
+            print(vetores)
 
         elif procedimento == 'correlacao_cruzada':
             vetores_correlacao_cruzada = input().split(',')
-            transforma_int(vetores_correlacao_cruzada)
-            correlacao_cruzada(list[int], vetores_correlacao_cruzada)
+            vetores_correlacao_cruzada = transforma_int(
+                vetores_correlacao_cruzada)
+            vetores = correlacao_cruzada(vetores, vetores_correlacao_cruzada)
+            print(vetores)
 
 
 if __name__ == "__main__":
