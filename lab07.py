dc51bbca7aecac9cfa7b0ffbef9e3d83420a0615
Lucas_Palacio <132016588+Palaciolol@users.noreply.github.com>
2023-05-12 23:53:27 -0300

    Add files via upload

--- lab07.py
+++ lab07.py
@@ -9,39 +9,44 @@ caractere2 = input() #Segundo caractere a ser procurado na string.
 
 linhas = int(input()) #Número de linhas da mensagem.
 
-soma_mensagem = ''
+mensagens = []
 
 
 for i in range(0,linhas):
     mensagem = input() #Mensagem a ser decodificada.
-    soma_mensagem += mensagem
+    mensagens.append(mensagem)
+
+achar_chave = ''.join(mensagens)
+
+
+
 
 
 def verificacao(caracter1: str)->int:
     
     def verificacao2(caractere2:str)->int:
         if caractere2 == 'vogal':
-            for c in range(x,len(soma_mensagem)):
-                if soma_mensagem[c] in 'AEIOUaeiou':
+            for c in range(x,len(achar_chave)):
+                if achar_chave[c] in 'AEIOUaeiou':
                     y = c
                     return y
 
         elif caractere2 == 'consoante':
-            for c in range(x,len(soma_mensagem)):
-                if soma_mensagem[i].isalpha():
-                    if soma_mensagem[c] not in 'AEIOUaeiou':
+            for c in range(x,len(achar_chave)):
+                if achar_chave[i].isalpha():
+                    if achar_chave[c] not in 'AEIOUaeiou':
                         y = c
                         return y
                         
         elif caractere2 == 'numero':
-            for c in range(x,len(soma_mensagem)):
-                if soma_mensagem[c] in '0123456789':
+            for c in range(x,len(achar_chave)):
+                if achar_chave[c] in '0123456789':
                     y = c
                     return y
 
 
         else:
-            y = soma_mensagem.find(caractere2)
+            y = achar_chave[x:].find(caractere2) + x
             return y
         
     
@@ -51,8 +56,8 @@ def verificacao(caracter1: str)->int:
     
     
     if caracter1 == 'vogal':
-        for i in range(0,len(soma_mensagem)):
-            if soma_mensagem[i] in 'AEIOUaeiou':
+        for i in range(0,len(achar_chave)):
+            if achar_chave[i] in 'AEIOUaeiou':
                 x = i
                 y = verificacao2(caractere2)
                 return (x, y)
@@ -60,23 +65,23 @@ def verificacao(caracter1: str)->int:
 
 
 
-    elif caracter1 == ' consoante':
-        for i in range(0,len(soma_mensagem)):
-            if soma_mensagem[i].isalpha():
-                if soma_mensagem[i] not in 'AEIOUaeiou':
+    elif caracter1 == 'consoante':
+        for i in range(0,len(achar_chave)):
+            if achar_chave[i].isalpha():
+                if achar_chave[i] not in 'AEIOUaeiou':
                     x = i
                     y = verificacao2(caractere2)
                     return (x, y)
     elif caracter1 == 'numero':
-        for i in range(0,len(soma_mensagem)):
-                if soma_mensagem[i] in '0123456789':
+        for i in range(0,len(achar_chave)):
+                if achar_chave[i] in '0123456789':
                     x = i
                     y = verificacao2(caractere2)
                     return (x, y)
 
 
     else:
-        x = soma_mensagem.find(caracter1)
+        x = achar_chave.find(caracter1)
         y = verificacao2(caractere2)
         return (x,y)
 
@@ -97,6 +102,17 @@ elif operacao == '*':
     chave = x * y
 
 print(chave)
+
+
+for m in mensagens:
+    msg_decodificada = ''
+    for c in m:
+        pos1 = (ord(c) + chave - 32) % 95 + 32
+        msg_decodificada += chr(pos1)
+                         
+    
+
+    print(msg_decodificada)
 
 
 
