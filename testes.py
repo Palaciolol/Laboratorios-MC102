f0db330d6ffb0da5b6f36aa17351dae46237eece
Palacio <lucaspalacioa@gmail.com>
2025-03-30 20:28:46 -0300

    ta dando bom?

--- testes.py
+++
@@ -1,4 +0,0 @@
-or mat[self.x + 1][self.y] == self.tipo
-                    or mat[self.x+1][self.y] == 'R'
-                    or mat[self.x+1][self.y] == 'L'
-                    or mat[self.x+1][self.y] == 'U'
